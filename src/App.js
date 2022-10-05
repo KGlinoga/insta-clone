888dd3cac965d7ad9c69d74d34ddeccba2ecb7ab
Kristina Faye <kglinoga@gmail.com>
2022-10-05 10:05:23 -0700

    added IG logo and background colors. fixed App className to be app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello! not yet deployed*</h1>
+    <div className="app">
+      
+      {/* Header */}
+      <header className="app__header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram logo"></img>
+      </header>
+          
+      <h1>hello! not yet deployed*</h1>
+      
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }

1567deb22e9f29761f07666ed9a3cbf4703accef
Kristina Faye <kglinoga@gmail.com>
2022-10-05 11:22:44 -0700

    WOW the power of organized and logical classNames. V impressed

--- src/Post.js
+++ src/Post.js
@@ -6,18 +6,20 @@ function Post() {
     return (
         <div className="post">
             {/* header -  avatar and username*/}
+            <div className="post__header">
             <Avatar
                 className="post__avatar" 
                 alt="heygoodlurking"
                 src="https://www.w3schools.com/howto/img_avatar2.png"
             />
             <h3>Username: heygoodlurking</h3>
-
+            </div>
+            
             {/* image */}
             <img className="post__image"
                 src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"
                 alt="mountains under mist"
-                ></img>
+                />
 
             {/* username and caption */}
             <h4 className="post__text"><strong>heygoodlurking:</strong> OMG this is awesome</h4>

793fba02822db5748615fba9f5f0d64a08817886
Kristina Faye <kglinoga@gmail.com>
2022-10-06 13:48:31 -0700

    started GH pages process

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from './Post';
 // import { db } from './firebase';
@@ -14,6 +14,11 @@ function App() {
       username: "DashTheDog",
       caption: "woof! I love to run!",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHCN1JCw8KMMokOY-JHDsZjA4eKgjq1ZM4Fw&usqp=CAU"
+    },
+    {
+      username:"heygoodlurking",
+      caption:"yay!",
+      imageUrl:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"
     }
   ]);
 
@@ -35,6 +40,8 @@ function App() {
           className="app_headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt="Instagram logo"></img>
+        
+        
       </header>
           
       <h1>hello! not yet deployed*</h1>
@@ -51,9 +58,9 @@ function App() {
       
       {/* Posts */}
 
-      <Post username="DashTheDog" caption="arf!" imageUrl="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"/>
+      {/* <Post username="DashTheDog" caption="arf!" imageUrl="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"/>
       <Post username="heygoodlurking" caption="yay!" imageUrl="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg" />
-      <Post username="PepperTheCat" caption="mi au!" imageUrl="https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-1280.jpg" />
+      <Post username="PepperTheCat" caption="mi au!" imageUrl="https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-1280.jpg" /> */}
 
     
     </div>
